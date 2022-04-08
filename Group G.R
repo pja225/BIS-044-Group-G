a1cc89615ca4020dc7422579a24978e4210e7c11
Peter Allen <cnt221@lehigh.edu>
2022-04-07 23:45:16 -0400

    Group G.R

---
+++ Group G.R
@@ -0,0 +1,7 @@
+View(nfl_pass_rush_receive_raw_data)
+library(tidyverse)
+DF <- (nfl_pass_rush_receive_raw_data)
+df
+data.frame(a1 = 1:3, a2 = 4:6, a3 = 7:9)
+cat ("The average of the a2 column is", "\n")
+mean(df$a2)
